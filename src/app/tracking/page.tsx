0ce06212670167e2e033a4a137ec30fa05d9712e
MARTIN ISAIAS LAGOS SEPULVEDA <135758492+mvtnDEV@users.noreply.github.com>
2026-04-26 20:25:19 -0400

    Delete src/app/tracking/page.tsx

--- src/app/tracking/page.tsx
+++
@@ -1,9 +0,0 @@
-import TrackingClient from './client'
-
-export default function TrackingPage({
-  searchParams
-}: {
-  searchParams: { q?: string }
-}) {
-  return <TrackingClient initialQuery={searchParams.q || ''} />
-}
