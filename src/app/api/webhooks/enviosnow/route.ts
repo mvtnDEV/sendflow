d3b89e9267f79349ad49b55ddfc2b596cf678711
MARTIN ISAIAS LAGOS SEPULVEDA <135758492+mvtnDEV@users.noreply.github.com>
2026-04-29 17:35:31 -0400

    Update route.ts

--- src/app/api/webhooks/enviosnow/route.ts
+++ src/app/api/webhooks/enviosnow/route.ts
@@ -125,7 +125,7 @@ export async function POST(req: NextRequest) {
             create: {
               status:    newStatus as any,
               note,
-              createdBy: 'system',
+              createdBy: 'null',
             },
           },
         },
