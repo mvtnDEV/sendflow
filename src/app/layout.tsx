8ac38715c9d9cbeb9f6785da6e858b0edfcbbf1c
MARTIN ISAIAS LAGOS SEPULVEDA <135758492+mvtnDEV@users.noreply.github.com>
2026-05-22 22:32:31 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import type { Metadata } from 'next'
 import './globals.css'
 export const dynamic = 'force-dynamic'
-
 export const metadata: Metadata = {
-  title: 'SendFlow',
+  title: 'Moovex',
   description: 'Sistema de gestión logística',
 }
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
